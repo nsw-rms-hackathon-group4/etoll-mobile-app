dfd45498781d54825d4aefa757095b71de7f7a31
Mohan Ambalavanan <mohan.ambalavanan@gmail.com>
2015-04-21 23:20:49 +1000

    working on geolocations

--- www/js/etoll.js
+++ www/js/etoll.js
@@ -9,29 +9,171 @@
         });
     });
 
-    app.service('backgroundGeolocationService', function ($timeout, $window, $cordovaLocalNotification) {
-        this.onSuccess = function (position) {
-            console.log('Latitude: ' + position.coords.latitude + '\n' +
-            'Longitude: ' + position.coords.longitude + '\n' +
-            'Altitude: ' + position.coords.altitude + '\n' +
-            'Accuracy: ' + position.coords.accuracy + '\n' +
-            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
-            'Heading: ' + position.coords.heading + '\n' +
-            'Speed: ' + position.coords.speed + '\n' +
-            'Timestamp: ' + position.timestamp + '\n');
-        };
+    app.service('backgroundGeolocationService', function ($timeout, $window, $interval, $cordovaLocalNotification, $cordovaGeolocation) {
+            var notification = 1000;
+            var posOptions = {timeout: 10000, enableHighAccuracy: false};
+            var self = this;
 
-        this.configureBackgroundTask = function () {
+            this.getCurrentPosition = function () {
+                return $cordovaGeolocation
+                    .getCurrentPosition(posOptions);
 
-        };
+            }
 
-        this.onError =
-            function (error) {
-                console.log('code: ' + error.code + '\n' +
-                'message: ' + error.message + '\n');
+            this.addNotification = function (message) {
+                $cordovaLocalNotification.add({
+                    id: notification,
+                    message: message
+                });
+                notification++;
             };
 
-    });
+            this.startTracking = function () {
+                alert(" I am trying to track...");
+                return self.getCurrentPosition().then(function (position) {
+                    (function () {
+                        "use strict";
+                        var app = angular.module('eTollGeoLocation', ['ionic', 'ngCordova']);
+
+                        app.run(function ($ionicPlatform, backgroundGeolocationService) {
+                            $ionicPlatform.ready(function () {
+
+                                backgroundGeolocationService.configureBackgroundTask();
+                            });
+                        });
+
+                        app.service('backgroundGeolocationService', function ($timeout, $window, $interval, $cordovaLocalNotification, $cordovaGeolocation) {
+                                var notification = 1000;
+                                var posOptions = {timeout: 10000, enableHighAccuracy: false};
+                                var self = this;
+
+                                this.getCurrentGeoLocation = function () {
+                                    console.log("Geo location %s",  $cordovaGeolocation
+                                        .getCurrentPosition(posOptions));
+                                     $cordovaGeolocation
+                                        .getCurrentPosition(posOptions).then(function(position){
+                                             var lat = position.coords.latitude;
+                                             var long = position.coords.longitude;
+                                             console.log("Position is :" + position);
+                                             self.addNotification(lat + long);
+                                         },function(error){
+
+                                         });
+                                };
+
+                                this.addNotification = function (message) {
+                                    $cordovaLocalNotification.add({
+                                        id: notification,
+                                        message: message
+                                    });
+                                    notification++;
+                                };
+
+                                this.startTracking = function () {
+                                    alert(" I am trying to tracksss...");
+                                    self.getCurrentGeoLocation();
+                                };
+
+                                this.configureBackgroundTask = function () {
+                                    console.log("Configuring Background Task");
+                                    window.plugin.backgroundMode.setDefaults({text: 'Tracking Toll on Background'});
+                                    window.plugin.backgroundMode.enable();
+                                    window.plugin.backgroundMode.onactivate = function () {
+                                        console.log("Inside Background Task");
+                                        $timeout(function () {
+
+                                            window.plugin.backgroundMode.configure({
+                                                text: "Tracking Toll Locations on Background"
+                                            });
+
+                                        }, 5000);
+                                        $interval(self.startTracking, 5000);
+                                    };
+                                    window.plugin.backgroundMode.onfailure = function (errorCode) {
+                                        alert(errorCode);
+                                    };
+
+                                };
+
+                                this.onError =
+                                    function (error) {
+                                        console.log('code: ' + error.code + '\n' +
+                                        'message: ' + error.message + '\n');
+                                    };
+
+                            }
+                        )
+                        ;
+                        app.controller('EtollGeoLocationController', function ($scope, $timeout, $cordovaBackgroundGeolocation,
+                                                                               $cordovaLocalNotification) {
+                            var options = {
+
+                                desiredAccuracy: 10,
+                                stationaryRadius: 20,
+                                distanceFilter: 30,
+                                notificationTitle: 'Background tracking', // <-- android only, customize the title of the notification
+                                notificationText: 'ENABLED', // <-- android only, customize the text of the notification
+                                activityType: 'AutomotiveNavigation',
+                                debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+                                stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+                            };
+
+                            // `configure` calls `start` internally
+                            //$cordovaBackgroundGeolocation.configure(options)
+                            //    .then(
+                            //    null, // Background never resolves
+                            //    function (err) { // error callback
+                            //        console.error(err);
+                            //    },
+                            //    function (location) { // notify callback
+                            //        console.log(location);
+                            //        $cordovaLocalNotification.add({
+                            //            id: '1',
+                            //            message: "Push!!" + i + location,
+                            //        });
+                            //    });
+                        });
+
+                    })
+                    ();
+                    var lat = position.coords.latitude;
+                    var long = position.coords.longitude;
+                    self.addNotification(lat + long);
+                }, function (error) {
+
+                });
+            };
+
+            this.configureBackgroundTask = function () {
+                console.log("Configuring Background Task");
+                window.plugin.backgroundMode.setDefaults({text: 'Tracking Toll on Background'});
+                window.plugin.backgroundMode.enable();
+                window.plugin.backgroundMode.onactivate = function () {
+                    console.log("Inside Background Task");
+                    $timeout(function () {
+
+                        window.plugin.backgroundMode.configure({
+                            text: "Tracking Toll Locations on Background"
+                        });
+
+                    }, 5000);
+                    $interval(self.startTracking, 5000);
+                };
+                window.plugin.backgroundMode.onfailure = function (errorCode) {
+                    alert(errorCode);
+                };
+
+            };
+
+            this.onError =
+                function (error) {
+                    console.log('code: ' + error.code + '\n' +
+                    'message: ' + error.message + '\n');
+                };
+
+        }
+    )
+    ;
     app.controller('EtollGeoLocationController', function ($scope, $timeout, $cordovaBackgroundGeolocation,
                                                            $cordovaLocalNotification) {
         var options = {
@@ -47,116 +189,20 @@
         };
 
         // `configure` calls `start` internally
-        $cordovaBackgroundGeolocation.configure(options)
-            .then(
-            null, // Background never resolves
-            function (err) { // error callback
-                console.error(err);
-            },
-            function (location) { // notify callback
-                console.log(location);
-                $cordovaLocalNotification.add({
-                    id: '1',
-                    message: "Push!!" + i + location,
-                });
-            });
+        //$cordovaBackgroundGeolocation.configure(options)
+        //    .then(
+        //    null, // Background never resolves
+        //    function (err) { // error callback
+        //        console.error(err);
+        //    },
+        //    function (location) { // notify callback
+        //        console.log(location);
+        //        $cordovaLocalNotification.add({
+        //            id: '1',
+        //            message: "Push!!" + i + location,
+        //        });
+        //    });
     });
-
-    angular.module('ionicApp', ['ionic'])
-
-        .config(function ($stateProvider, $urlRouterProvider) {
-
-            $stateProvider
-                .state('signin', {
-                    url: '/sign-in',
-                    templateUrl: 'templates/sign-in.html',
-                    controller: 'SignInCtrl'
-                })
-                .state('forgotpassword', {
-                    url: '/forgot-password',
-                    templateUrl: 'templates/forgot-password.html'
-                })
-                .state('Register', {
-                    url: '/Register',
-                    templateUrl: 'templates/Register.html',
-                    controller: 'SignInCtrl'
-                })
-                .state('forgotpasswordEmail', {
-                    url: '/forgot-passwordEmail',
-                    templateUrl: 'templates/forgot-passwordEmail.html'
-                })
-                .state('tabs', {
-                    url: '/tab',
-                    abstract: true,
-                    templateUrl: 'templates/tabs.html'
-                })
-                .state('tabs.home', {
-                    url: '/home',
-                    views: {
-                        'home-tab': {
-                            templateUrl: 'templates/home.html',
-                            controller: 'HomeTabCtrl'
-                        }
-                    }
-                })
-                .state('tabs.facts', {
-                    url: '/facts',
-                    views: {
-                        'home-tab': {
-                            templateUrl: 'templates/facts.html'
-                        }
-                    }
-                })
-                .state('tabs.facts2', {
-                    url: '/facts2',
-                    views: {
-                        'home-tab': {
-                            templateUrl: 'templates/facts2.html'
-                        }
-                    }
-                })
-                .state('tabs.about', {
-                    url: '/about',
-                    views: {
-                        'about-tab': {
-                            templateUrl: 'templates/about.html'
-                        }
-                    }
-                })
-                .state('tabs.navstack', {
-                    url: '/navstack',
-                    views: {
-                        'about-tab': {
-                            templateUrl: 'templates/nav-stack.html'
-                        }
-                    }
-                })
-                .state('tabs.contact', {
-                    url: '/contact',
-                    views: {
-                        'contact-tab': {
-                            templateUrl: 'templates/contact.html'
-                        }
-                    }
-                });
-
-
-            $urlRouterProvider.otherwise('/sign-in');
-
-        })
-
-        .controller('SignInCtrl', function ($scope, $state) {
-
-            $scope.signIn = function (user) {
-                console.log('Sign-In', user);
-                $state.go('tabs.home');
-            };
-
-        })
-
-        .controller('HomeTabCtrl', function ($scope) {
-            console.log('HomeTabCtrl');
-        });
 
 })
 ();
