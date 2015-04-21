ca7126680811eb9ba1dd4a146d5d39ee50a9d5f9
mohan82 <megeek69>
2015-04-21 11:07:22 +1000

    Hacking Geolocation

--- www/js/etoll.js
+++ www/js/etoll.js
@@ -1,11 +1,29 @@
-
 angular.module('eTollTask1', ['ionic'])
 
-    .controller('TodoCtrl', function($scope) {
-        $scope.tasks = [
-            { title: 'Collect coins' },
-            { title: 'Eat mushrooms' },
-            { title: 'Get high enough to grab the flag' },
-            { title: 'Find the Princess' }
-        ];
+    .controller('TodoCtrl', function ($scope) {
+        $scope.onSuccess = function (position) {
+            alert('Latitude: ' + position.coords.latitude + '\n' +
+            'Longitude: ' + position.coords.longitude + '\n' +
+            'Altitude: ' + position.coords.altitude + '\n' +
+            'Accuracy: ' + position.coords.accuracy + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+            'Heading: ' + position.coords.heading + '\n' +
+            'Speed: ' + position.coords.speed + '\n' +
+            'Timestamp: ' + position.timestamp + '\n');
+        };
+
+     $scope.onError = function (error) {
+            alert('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+        };
+        console.log(" I am here Ready");
+
+        ionic.Platform.ready(function () {
+            console.log(" I am here Ready");
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+        })
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
     });
